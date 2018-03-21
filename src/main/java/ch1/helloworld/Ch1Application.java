89e39ac83f623104d73e3cfa0676098f01d96c23
LeeGuoPing <xdliguoping@163.com>
2018-03-21 20:01:40 +0800

    自定义消息转换器

--- src/main/java/ch1/helloworld/Ch1Application.java
+++ src/main/java/ch1/helloworld/Ch1Application.java
@@ -2,8 +2,12 @@ package ch1.helloworld;
 
 
 
+import java.nio.charset.Charset;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.StringHttpMessageConverter;
 
 @SpringBootApplication
 public class Ch1Application{
@@ -13,6 +17,16 @@ public class Ch1Application{
 		System.out.println();
 		System.out.println("start succes!!!!!!!!!!!!!!!!!");
 		System.out.println();
+	}
+	
+	/**
+	 * 自定义消息转换器
+	 * 在spring boot中默认是utf-8不需要设置
+	 */
+	@Bean
+	public StringHttpMessageConverter stringHttpMessageConverter(){
+		StringHttpMessageConverter stringHttpMessageConverter = new StringHttpMessageConverter(Charset.forName("utf-8"));
+		return stringHttpMessageConverter;
 	}
 
 }
