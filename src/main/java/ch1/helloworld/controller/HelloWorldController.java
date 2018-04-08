a6864b8f242dfd76788d7a1354d321c06e317d95
LeeGuoPing <xdliguoping@163.com>
2018-04-08 23:59:55 +0800

    spelling

--- src/main/java/ch1/helloworld/controller/HelloWorldController.java
+++ src/main/java/ch1/helloworld/controller/HelloWorldController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class HelloworldController {
+public class HelloWorldController {
 	
 	@RequestMapping("/sayhello.html")
 	public @ResponseBody String say(String name){
